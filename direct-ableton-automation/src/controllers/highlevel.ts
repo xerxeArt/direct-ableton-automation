702866f759e4412810d3e2e70b3f83d54d8925c5
xerxeArt <cristi_lazar@yahoo.com>
2025-09-03 12:00:03 +0300

    Update highlevel.ts

---
+++ direct-ableton-automation/src/controllers/highlevel.ts
@@ -0,0 +1,24 @@
+// src/controllers/highlevel.ts
+import { LocatorSpec, TrackSpec } from "../types";
+
+// In future: import ableton from "ableton-js";
+// and lower layers: song.set("current_song_time"); song.call("set_or_delete_cue")
+
+export async function createLocators(locators: LocatorSpec[]): Promise<void> {
+  // TODO: ableton.song.* calls per locator.bar and locator.name
+}
+
+export type LiveTrackRef = { id: string; name: string };
+
+export async function createTracks(specs: TrackSpec[]): Promise<LiveTrackRef[]> {
+  // TODO: create groups, colors, track types; return LiveTrackRef[]
+  return specs.map((t, i) => ({ id: `track-${i}`, name: t.name }));
+}
+
+export async function addInstruments(
+  liveTracks: LiveTrackRef[],
+  specs: TrackSpec[]
+): Promise<number> {
+  // TODO: device chains, ExternalInstrument device setup
+  return specs.filter(s => s.device_chain || s.external_instrument).length;
+}
